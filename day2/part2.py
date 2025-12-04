44bb5333b8c14b9ec99d5ca61eaa3e2c6654c853
aaron-hwang <aaronhwang22@gmail.com>
2025-12-04 18:52:15 -0500

    day 2 part 2

--- day2/part2.py
+++ day2/part2.py
@@ -1,15 +1,17 @@
+import re
 res = 0
-with open('intput.txt', 'r') as file:
+with open("input.txt", 'r') as file:
     contents = file.read()
     ranges = contents.split(',')
     for range_ in ranges:
         temp = range_.split('-')
         lo = int(temp[0])
         hi = int(temp[1])
-        # brute force: check every number in range of digit length, check if string is 
-        # made of repetitions of it 
+        curr = []
         for i in range(lo, hi + 1):
-            temp = str(i)
-            for j in range(len(temp) // 2):
-                # TODO
-                pass
+            tmp = str(i)
+            if re.match(r"^(\d+)\1+$", tmp):
+                curr.append(i)
+                res += i
+        print(curr)
+print(res)
